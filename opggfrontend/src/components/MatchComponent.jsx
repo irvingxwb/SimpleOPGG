b75aafe7b7e29c86630a4295929cf24f591b6460
wenbin xiao <irvingx@sfu.ca>
2020-09-04 00:59:49 -0700

    support 50 records now and add index list under record, jumping function not finished

--- opggfrontend/src/components/MatchComponent.jsx
+++ opggfrontend/src/components/MatchComponent.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import {InputGroup, FormControl, Button} from "react-bootstrap";
+import React, {Component} from "react";
+import {InputGroup, FormControl, Button, ListGroup} from "react-bootstrap";
 import axios from "axios";
 import "./Login.css";
 import config from "../Config/server.json";
@@ -9,61 +9,72 @@ import MatchRecord from "./MatchRecordComponent";
 /* eslint-disable */
 
 class Match extends Component {
-  state = {
-    summonerid: React.createRef(),
-    match_list: [],
-    ShowRecord: false
-  };
+    state = {
+        summonerid: React.createRef(),
+        match_data: [],
+        display_data: [],
+        ShowRecord: false,
+        idx_cnt: 0,
+    };
 
 
+    render() {
+        return (
+            <div>
+                <div>{this.getSearchblock()}</div>
+                <div className="MatchList">{this.getMatchList()}/></div>
+                <div className="MatchIndex">{this.getIndex(this.state.idx_cnt)}</div>
+            </div>
+        );
+    }
 
-  render() {
-    return (
-      <div>
-        <div>{this.getSearchblock()}</div>
-        <div className="MatchList">{this.getMatchList()}/></div>
-      </div>
-    );
-  }
+    getIndex = (idx_count) => {
+        var index = (
+            <ListGroup horizontal>
+                {[...Array(idx_count).keys()].map(value => <ListGroup.Item>value</ListGroup.Item>)}
+            </ListGroup>)
+    }
 
-  getSearchblock() {
-    const search = (
-      <InputGroup className="search">
-        <FormControl ref={this.state.summonerid} type="text" />
-        <InputGroup.Append>
-          <Button variant="primary" onClick={this.handleSearch}>
-            Search
-          </Button>
-        </InputGroup.Append>
-      </InputGroup>
-    );
-    return search;
-  }
+    getSearchblock() {
+        const search = (
+            <InputGroup className="search">
+                <FormControl ref={this.state.summonerid} type="text"/>
+                <InputGroup.Append>
+                    <Button variant="primary" onClick={this.handleSearch}>
+                        Search
+                    </Button>
+                </InputGroup.Append>
+            </InputGroup>
+        );
+        return search;
+    }
 
-  getMatchList() {
-    const MatchList = (
-      <ul>
-        {this.state.ShowRecord && this.state.match_list.map(record =>
-        {
-          const ret = <MatchRecord key={record.gameDuration} data={record} playerid={this.state.summonerid.current.value}/>
-          return ret
-        }
-        )}
-      </ul>
-    );
-    return MatchList
-  }
+    getMatchList() {
+        const MatchList = (
+            <ul>
+                {this.state.ShowRecord && this.state.display_data.map(record => {
+                        const ret = <MatchRecord key={record.gameDuration} data={record}
+                                                 playerid={this.state.summonerid.current.value}/>
+                        return ret
+                    }
+                )}
+            </ul>
+        );
+        return MatchList
+    }
 
-  handleSearch = async () => {
-    let url = config.server + config.matchdata
-    let params = new URLSearchParams()
-    params.append('summonername', this.state.summonerid.current.value)
-    params.append('region', 'na1')
+    handleSearch = async () => {
+        let url = config.server + config.matchdata
+        let params = new URLSearchParams()
+        params.append('summonername', this.state.summonerid.current.value)
+        params.append('region', 'na1')
 
-    let response = await axios.post(url, params)
-    this.setState({match_list: response.data.data})
-    this.setState({ShowRecord: true})
-  }
+        let response = await axios.post(url, params)
+        this.setState({match_data: response.data.data})
+        this.setState({display_data: response.data.data.slice(0, 5)})
+        this.setState({ShowRecord: true})
+        this.setState({idx_cnt: Math.ceil(response.data.data.length / 5)})
+    }
 
 }
 
